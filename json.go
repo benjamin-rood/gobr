ff5af6dd1090f79212c805098e6daf180a298062
benjamin-rood <bisr@icloud.com>
2016-02-09 11:50:18 +1300

    Simple, generic Input/Output wrappers for sending and receiving JSON struct types in Go.

---
+++ json.go
@@ -0,0 +1,15 @@
+package goio
+
+import "encoding/json"
+
+// InMsg – typestring wrapper for generic *received* msg
+type InMsg struct {
+	Type string          `json:"type"`
+	Data json.RawMessage `json:"data"`
+}
+
+// OutMsg - typestring wrapper for generic *exported* msg
+type OutMsg struct {
+	Type string      `json:"type"`
+	Data interface{} `json:"data"`
+}
