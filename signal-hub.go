e2422fe468b2ea25e946acfb55f37826cedf59c2
benjamin-rood <bisr@icloud.com>
2016-02-10 20:25:52 +1300

    Added boolean 'echo' flag to Broadcast and Signal methods which give the use the option of echoing the receiver signatures who are being Broadcasted or Signalled respectively. Wrote basic unit tests. Fixed a loop import typo in WaitForSignalOnce.

--- signal-hub.go
+++ signal-hub.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"sync"
-
-	"github.com/benjamin-rood/gobr"
 )
 
 // SignalHub is for simple non-blocking to signal receiver channels individually and broadcasting to all receivers
@@ -24,23 +22,28 @@ func NewSignalHub() *SignalHub {
 
 // Broadcast safely sends a signal to all receivers. By using invoking the method *as a goroutine* instead of sending channel directly, we avoid the chance of the sender blocking while waiting for the receiver to retreive the message sent on the channel.
 // This also means that we can Broadcast "as-if" there are processes waiting to receive the synchronisation signal, even though in some cases there might not be.
-func (sh *SignalHub) Broadcast() {
+func (sh *SignalHub) Broadcast(echo bool) {
 	defer sh.rw.Unlock()
 	sh.rw.Lock()
 	for signature, channel := range sh.receivers { // if map is empty then no signals get sent or lost.
-		fmt.Println("broadcasting to", signature)
+		if echo {
+			fmt.Println("broadcasting to", signature)
+		}
 		channel <- struct{}{}
 	}
 }
 
 // Signal 'safely' sends a signal message (struct{}{}) without blocking the sender *if invoked as a goroutine*.
 // Otherwise you need to check for the error if you want to confirm receipt.
-func (sh *SignalHub) Signal(signature string) error {
+func (sh *SignalHub) Signal(signature string, echo bool) error {
 	defer sh.rw.Unlock()
 	sh.rw.Lock()
 	_, ok := sh.receivers[signature]
 	if !ok {
 		return errors.New("SignalHub.Signal() failed: receiver with key (signature string passed to method) does not exist")
+	}
+	if echo {
+		fmt.Println("signalling", signature)
 	}
 	sh.receivers[signature] <- struct{}{}
 	return nil
@@ -70,10 +73,10 @@ func (sh *SignalHub) Deregister(signature string) {
 
 // WaitForSignalOnce - single-use operation in one tidy function:
 // registers, blocks until signalled received once once, then deregisters on return.
-func WaitForSignalOnce(s string, ts *gobr.SignalHub) {
+func WaitForSignalOnce(s string, ts *SignalHub) {
 	defer ts.Deregister(s)
-	signal, ok := ts.Register(s)
-	if !ok {
+	signal, clash := ts.Register(s)
+	if clash {
 		return
 	}
 	<-signal // block while waiting for signal to be received.
--- signal-hub_test.go
+++ signal-hub_test.go
@@ -1,1 +1,99 @@
 package gobr
+
+// These tests really depend on panicking or not... so I guess I need to grab
+// any panic and use it as the error for t.Errorf... but I can't be bothered
+// yet!
+// Currently it meets the requirements of `go test`, because if there is
+// a panic, then test will FAIL and refer to the function where the panic
+// occured as the test which failed.
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestSignalHub(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	quit := make(chan struct{})
+	sh := NewSignalHub()
+
+	for i := 'A'; i < 'G'; i++ { // i.e. 'A', 'B", ··· , 'F'
+		go func(signature string, hub *SignalHub) {
+			defer hub.Deregister(signature)
+			ch, _ := hub.Register(signature)
+			for {
+				select {
+				case <-quit:
+					return
+				case <-ch:
+					// fmt.Println(signature + " received signal")
+				}
+			}
+		}(string(i), sh)
+	}
+
+	time.Sleep(25 * time.Millisecond)
+	// fmt.Println("starting..")
+	sh.Broadcast(false)
+	// keeps main from exiting until this function literal returns
+	func(hub *SignalHub) {
+		rt := randomTicking(5, 100)
+		signature := string(0x41 + rand.Intn(6)) // randomly signal a receiver with string key directly.
+		timer := time.NewTimer(1 * time.Second)
+		for {
+			select {
+			case <-rt:
+				// fmt.Println("sending to", signature)
+				hub.Signal(signature, false)
+				signature = string(0x41 + rand.Intn(6)) // randomly pick rew receiver
+			case <-timer.C:
+				// fmt.Println("quitting...")
+				close(quit)
+				time.Sleep(25 * time.Millisecond)
+				return
+			}
+		}
+	}(sh)
+}
+
+func randomTicking(min, max int) chan struct{} {
+	rt := make(chan struct{})
+	go func(rt chan struct{}) {
+		d := time.Millisecond * time.Duration(min+rand.Intn(max-min))
+		// fmt.Println("rt burst in", d)
+		for {
+			select {
+			case <-time.After(d):
+				rt <- struct{}{}
+				d = time.Millisecond * time.Duration(min+rand.Intn(max-min))
+				// fmt.Println("rt burst in", d)
+			}
+		}
+	}(rt)
+	return rt
+}
+
+func TestWaitForSignalOnce(t *testing.T) {
+	quit := make(chan struct{})
+	sh := NewSignalHub()
+	go func() {
+		WaitForSignalOnce("TEST_WAIT_ONCE", sh)
+		close(quit)
+	}()
+	time.Sleep(5 * time.Millisecond)
+	sh.Signal("TEST_WAIT_ONCE", false)
+	<-quit // blocks until quit closed
+}
+
+func TestWaitForBroadcastOnce(t *testing.T) {
+	quit := make(chan struct{})
+	sh := NewSignalHub()
+	go func() {
+		WaitForSignalOnce("TEST_WAIT_ONCE", sh)
+		close(quit)
+	}()
+	time.Sleep(5 * time.Millisecond)
+	sh.Broadcast(false)
+	<-quit
+}
